0d5fcf6a9c3a7e8d165edc5854c186b40f8307d0
Billy <billybofh@gmail.com>
2020-11-06 13:30:45 +0000

    Add a dummy test to check mysql in github

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,9 +4,20 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
+    /** @test */
+    public function we_can_create_a_user_in_the_db()
+    {
+        $user = User::factory()->create(['email' => 'test@example.com']);
+
+        $this->assertEquals('test@example.com', $user->email);
+    }
+
     /**
      * A basic test example.
      *
